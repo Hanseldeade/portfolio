27f6d89e250be0bb1bf938ca56eefaacb011f097
Hanseldeade <henselljamesdeade@gmail.com>
2025-04-08 23:57:05 +0800

    Push

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -9,20 +9,13 @@ const Contact = () => {
 
       <div className="mt-10 flex gap-6 justify-center md:flex-row flex-col">
         <a
-          href="https://www.instagram.com/brendaoak/"
+          href="https://www.instagram.com/jaikasangual/"
           target="_blank"
           rel="noopener noreferrer"
           className=""
         >
-          <button className="relative transition-all ease-in-out duration-300 hover:scale-[1.1]">
-            <img
-              src="/button.webp"
-              alt="Button"
-              className="select-none w-44 invert"
-            />
-            <p className="absolute text-black z-10 top-0 left-0 right-0 bottom-0 m-auto h-fit">
-              Instagram
-            </p>
+          <button className="relative transition-all ease-in-out duration-300 hover:scale-[1.1] bg-white text-black px-6 py-2 rounded">
+            Instagram
           </button>
         </a>
 
@@ -32,27 +25,13 @@ const Contact = () => {
           rel="noopener noreferrer"
           className=""
         >
-          <button className="relative transition-all ease-in-out duration-300 hover:scale-[1.1]">
-            <img
-              src="/button.webp"
-              alt="Button"
-              className="select-none w-44 invert"
-            />
-            <p className="absolute text-black z-10 top-0 left-0 right-0 bottom-0 m-auto h-fit">
-              WhatsApp
-            </p>
+          <button className="relative transition-all ease-in-out duration-300 hover:scale-[1.1] bg-white text-black px-6 py-2 rounded">
+            WhatsApp
           </button>
         </a>
 
-        <button className="relative transition-all ease-in-out duration-300 hover:scale-[1.1] text-black md:mx-0 w-fit mx-auto">
-          <img
-            src="/button.webp"
-            alt="Button"
-            className="select-none w-44 invert"
-          />
-          <p className="absolute text-black z-10 top-0 left-0 right-0 bottom-0 m-auto h-fit text-sm">
-            +55 21 97723-6909
-          </p>
+        <button className="relative transition-all ease-in-out duration-300 hover:scale-[1.1] bg-white text-black px-6 py-2 rounded">
+          +55 21 97723-6909
         </button>
       </div>
     </footer>
